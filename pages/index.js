14e3278a807fd4c8c5835f0057bb8348aec0526f
melbynrojo <101743782+melbynrojo@users.noreply.github.com>
2023-02-10 13:31:33 -0500

    Metodo Post

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
   const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5')
+    getFoto()
+  }, [])
+
+  const person = [
+    { name: "Paulo", age: 22 },
+    { name: "Martin", age: 22, post: [{ id: 2123, name: "Mart", url: "www.photo" }, { id: 343, name: "Angela", url: "oo.photo" }] },
+    { name: "Pablo", age: 22 },
+  ]
+  console.log(person[1].post[1].name)
+
+  /* 
+  return (
+    <div>
+    {person[1].post.map((element) =>{
+  return(
+  <p>{element.name}</p>
+  ))
+    }}
+    <div/>
+  
+  )
+  */
+  /* METODO GET */
+  const getFoto = async () => {
+    await fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5')
       .then((resp) => resp.json())
       .then((info) => setData(info))
       .catch((err) => console.log(err))
-  }, [])
+  }
+  /* async/await cuando la funcion pudiera tardarse en ejecutarse */
+
+  /* METODO POST */
+  const postPost = (dataPost) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataPost) /* SIEMPRE VA A IR ESTO */
+    })
+      .then((resp) => resp.json())
+      .then((info) => console.log(info))
+      .catch((err) => console.log(err))
+  }
+
   console.log(data)
   return (
     <div>
-      {data?.map((element) => {
+      {person[1].post.map((element) => {
         return (
-          <img src={element?.url} width={600} height={600} />
+          <p>{element.name}</p>
         )
       })}
     </div>
   )
-    }
+}
